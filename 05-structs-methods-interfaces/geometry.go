cb18dd1427881b1916cf85f5d66e5f9943288a3f
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-06-16 13:16:03 +1200

    Finish lesson 5

--- 05-structs-methods-interfaces/geometry.go
+++ 05-structs-methods-interfaces/geometry.go
@@ -7,6 +7,10 @@ type Rectangle struct {
 	Height float64
 }
 
+type Shape interface {
+	Area() float64
+}
+
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
@@ -17,6 +21,15 @@ type Circle struct {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
 
 func Perimeter(rectangle Rectangle) float64 {
